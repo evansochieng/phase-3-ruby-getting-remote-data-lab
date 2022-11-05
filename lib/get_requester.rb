62f0b91090740e93787dcd6c37c76bd8ba2053fb
Evans Ochieng <evansochieng121@gmail.com>
2022-11-05 19:30:46 +0300

    practice making get requests to API

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,34 @@
 # Write your code here
+
+#require libraries that will be needed
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+#Create a GetRequester class
+class GetRequester
+    #initialize class with string url
+    def initialize(url)
+        @url = url
+    end
+
+    #method to make get request => return body of response
+    def get_response_body
+        #parse the url
+        uri = URI.parse(@url)
+        #make request
+        response = Net::HTTP.get_response(uri)
+
+        #return the body of the response
+        response.body
+    end
+
+    #parse the json response from get request
+    def parse_json
+        #catch response body
+        response = self.get_response_body
+
+        #parse JSON as ruby array/hash
+        JSON.parse(response)
+    end
+end
